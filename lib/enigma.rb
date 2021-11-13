7588ce737047aa43a2accf865b62ac956069bdc2
Seth Perna <gseth26@gmail.com>
2021-11-12 20:36:30 -0500

    finished encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,37 +6,35 @@ class Enigma
 
   def encrypt(message, key = key_generator, date = today_date)
     encryption = Hash.new(0)
-    offset = (date.to_i ** 2).to_s.slice(-4..-1).to_i #1025
+    offset = (date.to_i ** 2).to_s.slice(-4..-1).to_i
+    offset_2 = offset.to_s.split(//)#1025
     message_array = message.split(//)
     a_shift = key.slice(0..1).to_i #02
     b_shift = key.slice(1..2).to_i #27
     c_shift = key.slice(2..3).to_i #71
     d_shift = key.slice(3..4).to_i #15
     encrypted_message = []
-
-      message_array.each_with_index do |char, i|
-        if i % 4 == 0
-          encrypt_1 = @char_set.index(char) + (a_shift + offset[0]) #3
-            encrypted_message << @char_set[encrypt_1]
-        elsif i % 4 == 1
-          require "pry"; binding.pry
-          encrypt_2 = @char_set.index(char) + (b_shift + offset[1]) #27
-            encrypted_message << @char_set[encrypt_2]
-        elsif i % 4 == 2
-          encrypt_3 = @char_set.index(char) + (c_shift + offset[2]) #73
-            encrypted_message << @char_set[encrypt_3]
-        elsif i % 4 == 3
-          encrypt_4 = @char_set.index(char) + (d_shift + offset[3]) #20
-            encrypted_message << @char_set[encrypt_4]
-        end
+    message_array.each_with_index do |char, i|
+      if i % 4 == 0
+        encrypt_1 = @char_set.rotate((a_shift + offset_2[0].to_i))[@char_set.index(char)] #3
+          encrypted_message << encrypt_1
+      elsif i % 4 == 1
+        encrypt_2 = @char_set.rotate((b_shift + offset_2[1].to_i))[@char_set.index(char)] #27
+          encrypted_message << encrypt_2
+      elsif i % 4 == 2
+        encrypt_3 = @char_set.rotate((c_shift + offset_2[2].to_i))[@char_set.index(char)] #73
+          encrypted_message << encrypt_3
+      elsif i % 4 == 3
+        encrypt_4 = @char_set.rotate((d_shift + offset_2[3].to_i))[@char_set.index(char)] #20
+          encrypted_message << encrypt_4
       end
-      require "pry"; binding.pry
-      encrypted_message
-      encypytion = {
-        message: message,
-        key: key,
-        date: date
-      }
+    end
+    encrypted_message
+    encryption = {
+      message: encrypted_message.join,
+      key: key,
+      date: date
+    }
   end
 
   def key_generator
@@ -44,6 +42,6 @@ class Enigma
   end
 
   def today_date
-    Date.today.strftime("%m%d%y")
+    date = Date.today.strftime("%m%d%y")
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -16,10 +16,20 @@ RSpec.describe Enigma do
     expect(@enigma.char_set.count).to eq(27)
   end
 
+  it "has date" do
+
+    expect(@enigma.today_date).to eq("111221")
+  end
+
   it '#encrypt' do
     expected = {message: "keder ohulw", key: "02715", date: "040895"}
 
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
+  it '#encrypt without argument' do
+
+    expect(@enigma.encrypt("aghahjkdgjhsaf")).to eq(Hash)
   end
 
 end
