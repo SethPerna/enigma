a2531fac805be7498064e56235fbb76ad24beaeb
Seth Perna <gseth26@gmail.com>
2021-11-14 09:03:03 -0500

    more decrypt module  refactor

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -12,16 +12,16 @@ module Decrypt
     message_array.each_with_index do |char, i|
       char.downcase!
       if i % 4 == 0
-        encrypt_1 = @char_set.rotate((a_shift + offset_2[0].to_i) * -1)[@char_set.index(char)] #3
+        encrypt_1 = @char_set.rotate((a_shift + offset_2[0].to_i) * -1)[@char_set.index(char)]
           decrypted_message << encrypt_1
       elsif i % 4 == 1
-        encrypt_2 = @char_set.rotate((b_shift + offset_2[1].to_i) * -1)[@char_set.index(char)] #27
+        encrypt_2 = @char_set.rotate((b_shift + offset_2[1].to_i) * -1)[@char_set.index(char)]
           decrypted_message << encrypt_2
       elsif i % 4 == 2
-        encrypt_3 = @char_set.rotate((c_shift + offset_2[2].to_i) * -1)[@char_set.index(char)] #73
+        encrypt_3 = @char_set.rotate((c_shift + offset_2[2].to_i) * -1)[@char_set.index(char)]
           decrypted_message << encrypt_3
       elsif i % 4 == 3
-        encrypt_4 = @char_set.rotate((d_shift + offset_2[3].to_i) * -1)[@char_set.index(char)] #20
+        encrypt_4 = @char_set.rotate((d_shift + offset_2[3].to_i) * -1)[@char_set.index(char)] 
           decrypted_message << encrypt_4
       end
     end
