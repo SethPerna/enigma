e011f57258c412897917f63fcda213a868a4bdae
Seth Perna <gseth26@gmail.com>
2021-11-14 08:59:59 -0500

    rafactor

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,18 +1,15 @@
 require 'date'
 require './lib/enigma'
 require 'spec_helper'
-
 RSpec.describe Enigma do
   before :each do
     @enigma = Enigma.new
   end
   it 'exists' do
-
     expect(@enigma).to be_an_instance_of(Enigma)
   end
 
   it 'has #char_set' do
-
     expect(@enigma.char_set.count).to eq(27)
   end
 
