1a111c0cdc4ff26b7db0fe96f4530a70f99bd96d
Seth Perna <gseth26@gmail.com>
2021-11-14 10:06:16 -0500

    fixed runners to be dynamic

--- lib/encrypt_runner.rb
+++ lib/encrypt_runner.rb
@@ -1,9 +1,8 @@
 require './enigma'
 enigma = Enigma.new
-encryption = enigma.encrypt("Whats up FAM", "12345")
 input = ARGV[0]
 output = ARGV[1]
-ARGV[0] = File.read(input, chomp: true)
+encryption = enigma.encrypt(File.read(input).tr("\n", ""), "12345", "111421")
 File.open(output, "w") do |file|
   file.puts encryption[:message]
   puts "created #{output} with the key #{encryption[:key]} and date #{encryption[:date]}"
